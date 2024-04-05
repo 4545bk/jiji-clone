1b32cbae6575363918d1369e068fde064bb18cdf
Natnael Gizaw <116101430+4545bk@users.noreply.github.com>
2024-04-05 08:28:40 +0300

    Update firebase.js THE UNDERSCORE THIING

--- firebase.js
+++ firebase.js
@@ -7,12 +7,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT-PUBLIC-FIREBASE-API-KEY,
+  authDomain: process.env.NEXT-PUBLIC-FIREBASE-AUTH-DOMAIN,
+  projectId: process.env.NEXT-PUBLIC-FIREBASE-PROJECT-ID,
+  storageBucket: process.env.NEXT-PUBLIC-FIREBASE-STORAGE-BUCKET,
+  messagingSenderId: process.env.NEXT-PUBLIC-FIREBASE-MESSAGE-SENDER_ID,
+  appId: process.env.NEXT-PUBLIC-FIREBASE-APP-ID,
 };
 
 // Initialize Firebase
